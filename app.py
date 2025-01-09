84b612a4ce0a3df5ec8551f9cd863646971b37ac
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2025-01-09 13:23:42 -0300

    adiciona funcionalidade de perguntas e respostas com contexto usando LLM

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 import tempfile
+
 import streamlit as st
 
 from decouple import config
 
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma  import Chroma
+from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_openai import OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 
 os.environ['OPENAI_API_KEY'] = config('OPENAI_API_KEY')
@@ -51,12 +55,42 @@ def add_to_vector_store(chunks, vector_store=None):
         )
     return vector_store        
 
+def ask_question(model, query, vector_store):
+    llm = ChatOpenAI(model=model)
+    retriever = vector_store.as_retriever()
+
+    system_prompt = '''
+    Use o contexto para responder as perguntas.
+    Se não encontrar uma resposta no contexto,
+    explique que não há informações disponíveis.
+    Responda em formato de markdown e com visualizações
+    elaboradas e interativas.
+    Contexto: {context}
+    '''
+    messages = [('system', system_prompt)]
+    for message in st.session_state.messages:
+        messages.append((message.get('role'), message.get('content')))
+    messages.append(('human', query))
+
+    prompt = ChatPromptTemplate.from_messages(messages)  
+
+    question_answer_chain = create_stuff_documents_chain(
+        llm=llm,
+        prompt=prompt,
+    )
+    chain = create_retrieval_chain(
+        retriever=retriever,
+        combine_docs_chain=question_answer_chain,
+    )
+    response = chain.invoke({'input': query})
+    return response.get('answer')
 
 vector_store = load_existing_vector_store()
 
 st.set_page_config(
     page_title='Rag Chatbot',
     page_icon='🤖',
+    layout="centered",
 ),
 
 st.header('🤖 Chat com seus documentos RAG')
@@ -89,15 +123,29 @@ with st.sidebar:
         'gpt-4o',
     ]
     selected_model = st.sidebar.selectbox(
-        label='Selecione o modelo LLM',
-        options=model_options,
-    ),
+    label='Selecione o modelo LLM',
+    options=model_options,
+    )
 
 if 'messages' not in st.session_state:
     st.session_state['messages'] = []
-    
+
 
 question = st.chat_input('Como posso te ajudar?')
 
 if vector_store and question:
-    ...
+    for message in st.session_state.messages:
+        st.chat_message(message.get('role')).write(message.get('content'))
+    
+    st.chat_message('user').write(question)
+    st.session_state.messages.append({'role': 'user', 'content': question})
+
+    with st.spinner('Buscando resposta...'):
+        response = ask_question(
+            model=selected_model,
+            query=question,
+            vector_store=vector_store,
+        )
+
+        st.chat_message('ai').markdown(response)
+        st.session_state.messages.append({'role': 'ai', 'content': response})
